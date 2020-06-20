954a8af833b70b2171ab142288947a4eca5c4d84
darcdev <diegoandresrojas2000@gmail.com>
2020-06-19 21:07:21 -0500

    deep in react

--- react/src/js/components/App.js
+++ react/src/js/components/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState } from 'react';
+import data from '../../../data.json';
+import Loader from './Loader';
+
 
 function App(){
-    return 'SOy un Parrafor desde react';
+    const [loaderList , setLoaderList] = useState([]);
+    function handleCLick(){
+        setLoaderList(data.loaders);
+    }
+    return (<div>
+        <ul>
+            {
+                loaderList.map((item) => <Loader data={item} key={item.id}/>)
+            }
+        </ul>
+        <button onClick={handleCLick}>Mostrar lo aprendido hasta el momento</button>
+    </div>);
 }
 
 export default App;
