bda8573704b256bd6e82e9f8ffc8ed10a34f0f4c
darcdev <diegoandresrojas2000@gmail.com>
2020-06-19 20:32:27 -0500

    add plugin react babel

---
+++ react/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode : 'development',
+    entry : { 
+        "home" : path.resolve(__dirname ,'src/js/index.js'),
+    },
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename : 'js/[name].js'
+    },
+    devServer : {
+        open : true,
+        port : 9000,
+    },
+    module : {
+        rules : [
+            {
+                test : /\.css$/,
+                use : [ 'style-loader','css-loader']
+            },
+            {
+                test : /\.js$/,
+                use : 'babel-loader',
+                exclude : /node_modules/
+            }
+        ]
+    },
+    plugins : [
+         new MiniCssExtractPlugin({
+             filename : 'css/[name].css'
+         }),
+         new HtmlWebpackPlugin({
+             title : 'React',
+             template : path.resolve(__dirname , 'index.html')
+         })
+
+    ]
+
+}
